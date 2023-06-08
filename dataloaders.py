83b52e90bb9202a45d441ccebfb2851b671426ca
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2023-06-08 13:58:18 +0100

    Add new all-monai dataloader for 2D

--- dataloaders.py
+++ dataloaders.py
@@ -2,136 +2,99 @@ from torch.utils.data import DataLoader, Dataset
 from torch.nn.functional import interpolate
 from pythae.data.datasets import DatasetOutput
 import custom_monai as cmn
+import torch
 import torchio as tio
 import monai as mn
 import numpy as np
 import os, glob
 
 img_list = glob.glob('oasis/*/aligned_norm.nii.gz')
-mb_list = glob.glob('oasis/*/aligned_seg35.nii.gz')
+# mb_list = glob.glob('oasis/*/aligned_seg35.nii.gz')
 
 img_list_train, img_list_val = img_list[:int(0.8*len(img_list))], img_list[int(0.8*len(img_list)):]
-mb_list_train, mb_list_val = mb_list[:int(0.8*len(mb_list))], mb_list[int(0.8*len(mb_list)):]
+# mb_list_train, mb_list_val = mb_list[:int(0.8*len(mb_list))], mb_list[int(0.8*len(mb_list)):]
 
-class Object(object):
-    pass
 
-class CustomQueue(tio.Queue):
-    def __init__(
-            self,
-            subjects_dataset,
-            max_length: int,
-            samples_per_volume: int,
-            sampler,
-            num_workers: int = 0,
-            shuffle_subjects: bool = True,
-            shuffle_patches: bool = True,
-            start_background: bool = True,
-            verbose: bool = False,
-    ):
-        self.subjects_dataset = subjects_dataset
-        self.max_length = max_length
-        self.shuffle_subjects = shuffle_subjects
-        self.shuffle_patches = shuffle_patches
-        self.samples_per_volume = samples_per_volume
-        self.sampler = sampler
-        self.subject_sampler = sampler
-        self.num_workers = num_workers
-        self.verbose = verbose
-        self._subjects_iterable = None
-        self._incomplete_subject = None
-        self._num_patches_incomplete = 0
-        self._num_sampled_subjects = 0
-        if start_background:
-            self._initialize_subjects_iterable()
-        self.patches_list = []
-    
-    def __getitem__(self, _):
-        if not self.patches_list:
-            self._print('Patches list is empty.')
-            self._fill()
-            self.patches_list.reverse()
-        sample_patch = self.patches_list.pop()
-        # return {'data': sample_patch['data'][tio.DATA][...,0]}
-        return DatasetOutput(data=sample_patch['data'][tio.DATA][...,0])
-  
-
-class SliceDataset(Dataset):
-    def __init__(self, tiodata):
-        self.tiodata = tiodata
-        self.cur_data = None
-        self.cur_ix = None
-        self.idx_freeze = 0
+class VAEDataset(Dataset):
+    def __init__(self, data):
+        self.data = data
 
     def __len__(self):
         return len(self.tiodata)
 
     def __getitem__(self, idx): # completely ignore given idx and instead iterate internally
-        if self.cur_ix is None or self.cur_ix >= int(self.cur_data.shape[-1]):
-            if self.idx_freeze >= self.__len__():
-                self.idx_freeze = 0
-            img_path = self.tiodata.__getitem__(self.idx_freeze)
-            self.idx_freeze += 1
-            data = self.transform(img_path)
-            self.cur_data = data
-            self.cur_ix = 0
-   
-        data = self.cur_data['data'][tio.DATA][...,self.cur_ix]
-        self.cur_ix += 1
-        
-        return DatasetOutput(data=data)
+        data = self.data.__getitem__(idx)
+        return DatasetOutput(data=data["image"], label=data["label"])
 
 
 def get_mri_data(device):
-    transforms = tio.Compose([
-        tio.ToCanonical(),
-        tio.Resample(),
-        tio.Resize(128),
-        tio.RandomFlip(axes=(0,1,2)),
-        tio.RandomFlip(axes=(0,1,2)),
-        tio.RandomFlip(axes=(0,1,2)),
-        tio.RandomElasticDeformation(),
-        tio.RandomAffine(scales=0.15, degrees=15, translation=10),
-        tio.RescaleIntensity(out_min_max=(0,1), percentiles=(0.5,99.5)),
+    transforms = mn.transforms.Compose([
+        mn.transforms.LoadImageD(keys=["image", "label"]),
+        mn.transforms.AsChannelFirstD(keys=["image", "label"]),
+        mn.transforms.SpacingD(keys=['image','label'], pixdim=1),
+        mn.transforms.CropForegroundD(keys=['image','label'], source_key='image'),
+        mn.transforms.ResizeD(keys=['image','label'], spatial_size=(128,128,128)),
+        mn.transforms.RandFlipD(keys=['image','label'], spatial_axis=0, prob=0.5),
+        mn.transforms.RandFlipD(keys=['image','label'], spatial_axis=1, prob=0.5),
+        mn.transforms.Rand2DElasticD(keys=['image','label'], spacing=(10,10), magnitude_range=(50,150),
+                                  rotate_range=30, shear_range=0.15, translate_range=0.5, scale_range=0.2,
+                                  padding_mode='constant', mode=('bilinear','nearest')),
+        mn.transforms.ScaleIntensityRangePercentilesd(keys="image", lower=0, upper=99.5, b_min=0, b_max=1),
     ])
 
-    subj_train = [tio.Subject(data=tio.ScalarImage(img)) for img in img_list_train]
-    subj_val = [tio.Subject(data=tio.ScalarImage(img)) for img in img_list_val]
+    subj_train = [{"image":img, "label":img.replace('norm','seg35')} for img in img_list_train]
+    subj_val = [{"image":img, "label":img.replace('norm','seg35')} for img in img_list_val]
 
-    data_train = tio.SubjectsDataset(subj_train, transform=transforms)
-    data_val = tio.SubjectsDataset(subj_val, transform=transforms)
-
-    patch_sampler = tio.UniformSampler(patch_size=(128,128,1))
-
-    data_train = CustomQueue(data_train, max_length=2048, samples_per_volume=128, sampler=patch_sampler, num_workers=len(os.sched_getaffinity(0)))
-    data_val = CustomQueue(data_val, max_length=2048, samples_per_volume=128, sampler=patch_sampler, num_workers=len(os.sched_getaffinity(0)))
+    data_train = VAEDataset(mn.data.Dataset(subj_train, transform=transforms))
+    data_val = VAEDataset(mn.data.Dataset(subj_val, transform=transforms))
 
     return data_train, data_val
 
 
 def get_synth_data(device):
-    transforms = tio.Compose([
-        tio.ToCanonical(),
-        tio.Resample(),
-        tio.Resize(128),
-        tio.RandomFlip(axes=(0,1,2)),
-        tio.RandomFlip(axes=(0,1,2)),
-        tio.RandomFlip(axes=(0,1,2)),
-        tio.RandomLabelsToImage(label_key='label', image_key='data'),
-        tio.RandomElasticDeformation(),
-        tio.RandomAffine(scales=0.15, degrees=15, translation=10),
-        tio.RescaleIntensity(out_min_max=(0,1), percentiles=(0.5,99.5)),
+    transforms = mn.transforms.Compose([
+        mn.transforms.LoadImageD(keys=["label"]),
+        mn.transforms.AsChannelFirstD(keys=["label"]),
+        mn.transforms.SpacingD(keys=['label'], pixdim=1),
+        GMMSynthD(mu=255, std=16, fwhm=5, gmm_fwhm=5),
+        mn.transforms.CropForegroundD(keys=['image','label'], source_key='image'),
+        mn.transforms.ResizeD(keys=['image','label'], spatial_size=(128,128,128)),
+        mn.transforms.RandFlipD(keys=['image','label'], spatial_axis=0, prob=0.5),
+        mn.transforms.RandFlipD(keys=['image','label'], spatial_axis=1, prob=0.5),
+        mn.transforms.Rand2DElasticD(keys=['image','label'], spacing=(10,10), magnitude_range=(50,150),
+                                  rotate_range=30, shear_range=0.15, translate_range=0.5, scale_range=0.2,
+                                  padding_mode='constant', mode=('bilinear','nearest')),
+        mn.transforms.ScaleIntensityRangePercentilesd(keys="image", lower=0, upper=99.5, b_min=0, b_max=1),
     ])
 
-    subj_train = [tio.Subject(label=tio.ScalarImage(img)) for img in img_list_train]
-    subj_val = [tio.Subject(label=tio.ScalarImage(img)) for img in img_list_val]
+    subj_train = [{"image":img, "label":img.replace('norm','seg35')} for img in img_list_train]
+    subj_val = [{"image":img, "label":img.replace('norm','seg35')} for img in img_list_val]
 
-    data_train = tio.SubjectsDataset(subj_train, transform=transforms)
-    data_val = tio.SubjectsDataset(subj_val, transform=transforms)
-
-    patch_sampler = tio.UniformSampler(patch_size=(128,128,1))
-
-    data_train = CustomQueue(data_train, max_length=2048, samples_per_volume=128, sampler=patch_sampler, num_workers=8)
-    data_val = CustomQueue(data_val, max_length=2048, samples_per_volume=128, sampler=patch_sampler, num_workers=8)
+    data_train = VAEDataset(mn.data.Dataset(subj_train, transform=transforms))
+    data_val = VAEDataset(mn.data.Dataset(subj_val, transform=transforms))
 
     return data_train, data_val
+
+
+class GMMSynthD:
+    def __init__(self, mu=255, std=16, fwhm=5, gmm_fwhm=5):
+        self.mu = mu
+        self.std = std
+        self.fwhm = fwhm
+        self.gmm_fwhm = gmm_fwhm
+        self.labmap = {
+            0:0, 1:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9,
+            10:10, 11:11, 12:12, 13:13, 14:14, 15:15, 16:16, 17:17, 18:18, 19:19,
+            20:1, 21:2, 22:3, 23:4, 24:5, 25:6, 26:7, 27:8, 28:9, 29:10,
+            30:14, 31:15, 32:16, 33:17, 34:18, 35:19
+        }
+
+    def __call__(self, data):
+        d = dict(data)
+        label = d["label"]
+        label.apply_(lambda val: self.labmap[val]) # map to symmetric mask
+        labels = [
+            mn.transforms.GaussianSmooth(np.random.uniform(0, self.gmm_fwhm))(torch.normal(np.random.uniform(0, self.mu), np.random.uniform(0, self.std), label.shape) * (label==i))
+                   for i in range(20)] # sample random intensities for each tissue and apply within-tissue blurring
+        d["image"] = mn.transforms.GaussianSmooth(np.random.uniform(0, self.fwhm))(torch.stack(labels,0).sum(0)) # sum all tissues and apply whole-image blurring
+        return d
