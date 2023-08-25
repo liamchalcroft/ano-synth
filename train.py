68109d7e195cca4069357047cd2bd230c75a1b7c
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2023-08-25 16:48:43 +0100

    Correct save path

--- train.py
+++ train.py
@@ -199,6 +199,6 @@ if __name__ =='__main__':
                     "lr": lr_scheduler.state_dict(),
                     "wandb": WandBID(wandb.run.id).state_dict()
                 },
-                os.path.join(args.logdir, args.name,'checkpoint_epoch={}.pt'.format(epoch)))
+                os.path.join(args.root, args.name,'checkpoint_epoch={}.pt'.format(epoch)))
             os.path.join(args.logdir, args.name)
             
