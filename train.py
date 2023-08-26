e4a60246fa696b8e312edf99328fc72acf00e3f8
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2023-08-26 01:07:42 +0100

    Change opt to AdamW

--- train.py
+++ train.py
@@ -132,7 +132,7 @@ if __name__ =='__main__':
         wandb.config.update(args)
     wandb.watch(model)
 
-    opt = torch.optim.Adam(model.parameters(), args.lr)
+    opt = torch.optim.AdamW(model.parameters(), args.lr)
     def lambda1(epoch):
         return (1 - epoch / args.epochs) ** 0.9
     lr_scheduler = LambdaLR(opt, lr_lambda=[lambda1])
