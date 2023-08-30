aa0fd9894d7615d846fbb5e22e97792ad3da2ba4
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2023-08-30 23:37:57 +0100

    Add mix rename

--- train.py
+++ train.py
@@ -36,6 +36,8 @@ if __name__ =='__main__':
     
     if args.synth:
         args.name = 'synth-' + args.name
+    elif args.mix:
+        args.name = 'mix-' + args.name
     
     os.makedirs(os.path.join(args.root, args.name), exist_ok=True)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
