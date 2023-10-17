47750966d144f7b658b7fbf7d132aafe3a213713
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2023-10-17 11:29:34 +0100

    CHange embedding for VQVAE

--- train.py
+++ train.py
@@ -129,7 +129,8 @@ if __name__ =='__main__':
             num_channels=(16,16,32,64,128,128),
             num_res_layers=2,
             num_res_channels=(16,16,32,64,128,128),
-            embedding_dim=128,
+            num_embeddings=32,
+            embedding_dim=64,
             downsample_parameters=tuple(6*[(2, 4, 1, 1)]),
             upsample_parameters=tuple(6*[(2, 4, 1, 1, 0)]),
         ).to(device)
