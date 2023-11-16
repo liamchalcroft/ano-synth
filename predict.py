ff815a9eed1b5deb70c288a3825d764a3f7597b1
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2023-11-16 13:26:17 +0000

    ++

--- predict.py
+++ predict.py
@@ -121,10 +121,10 @@ if __name__ =='__main__':
 
     load = mn.transforms.LoadImageD(keys=["image"])
     transforms = mn.transforms.Compose([
-        mn.transforms.EnsureChannelFirstD(keys=["image"]),
-        mn.transforms.OrientationD(keys=["image"], axcodes="RAS"),
         mn.transforms.ToTensorD(keys=["image"], 
                                 dtype=float),
+        mn.transforms.EnsureChannelFirstD(keys=["image"]),
+        mn.transforms.OrientationD(keys=["image"], axcodes="RAS"),
         mn.transforms.SpacingD(keys=["image"], pixdim=(1,1,-1), mode=["bilinear"]),
         mn.transforms.ResizeD(keys=["image"], spatial_size=(224,224,-1), mode=["bilinear"]),
         mn.transforms.ScaleIntensityRangePercentilesd(keys="image", lower=0, upper=99.5, b_min=0, b_max=1, clip=True),
