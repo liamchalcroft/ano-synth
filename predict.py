883da395b9d961e188de81bb833ad13ae3c126fe
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2023-12-04 18:53:06 +0000

    ++

--- predict.py
+++ predict.py
@@ -126,7 +126,7 @@ if __name__ =='__main__':
 
     recon_scores = []
 
-    for item in tqdm.tqdm(img_list, total=len(img_list)):
+    for i,item in tqdm.tqdm(enumerate(img_list), total=len(img_list)):
         image = nb.load(item["image"]) # nibabel image
         unmodified_item = {"image": image.get_fdata()[None], "fname": item["fname"]}
         item = transforms(unmodified_item)
@@ -193,10 +193,11 @@ if __name__ =='__main__':
         reconstruction *= np.percentile(img, 99.5)
         reconstruction = reconstruction.astype(img.dtype)
 
-        nb.save(nb.Nifti1Image(reconstruction[0], image.affine, image.header), 
-                os.path.join(odir, fname+".nii.gz"))
-        nb.save(nb.Nifti1Image(anomaly[0], image.affine, image.header), 
-                os.path.join(odir, "ANOMALY_"+fname+".nii.gz"))
+        if i<=5:
+            nb.save(nb.Nifti1Image(reconstruction[0], image.affine, image.header), 
+                    os.path.join(odir, fname+".nii.gz"))
+            nb.save(nb.Nifti1Image(anomaly[0], image.affine, image.header), 
+                    os.path.join(odir, "ANOMALY_"+fname+".nii.gz"))
 
     myFile = open(os.path.join(odir, 'scores.csv'), 'w')
     writer = csv.writer(myFile)
